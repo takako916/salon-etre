75c4fe79265581837625da92a7522decc12d18b7
Takako ONODERA <takako.onodera@gmail.com>
2025-07-14 16:15:36 +0200

    SEO modified  Changes to be committed: 	modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,9 @@ import Navbar from "@/components/Navbar";
 export const metadata = {
   title: "Salon de l'être Blavozy 2025 | Bien-être Haute-Loire 43",
   description:
-    "Salon de l'être à Blavozy (43, Haute-Loire) : bien-être, massage, réflexologie, énergétique, lithothérapie, coaching, conférences, ateliers, art divinatoire, tarot, astrologie, développement personnel, soins naturels, spiritualité, et plus. Proche Le Puy-en-Velay.",
+    "Salon de l'être à Blavozy (43, Haute-Loire) : bien-être, massage, réflexologie, énergétique, lithothérapie, coaching, conférences, ateliers, art divinatoire, tarot, astrologie, développement personnel, soins naturels, spiritualité, et plus. Proche Le Puy-en-Velay (Le Puy en Velay).",
   keywords:
-    "salon bien-être, Blavozy, Haute-Loire, 43, Le Puy-en-Velay, massage, réflexologie, énergétique, lithothérapie, coaching, conférences, ateliers, art divinatoire, tarot, astrologie, développement personnel, soins naturels, spiritualité, médium, magnétiseur, sexothérapie, cartomancie",
+    "salon bien-être, Blavozy, Haute-Loire, 43, Le Puy-en-Velay, Le Puy en Velay, massage, réflexologie, énergétique, lithothérapie, coaching, conférences, ateliers, art divinatoire, tarot, astrologie, développement personnel, soins naturels, spiritualité, médium, magnétiseur, sexothérapie, cartomancie",
   verification: {
     google: "qCUSiVJLqyIsWSPIvMek8WU-XOaFbLoxe3J_WGvN9Gg",
   },
@@ -16,7 +16,7 @@ export const metadata = {
     title: "Salon de l'être Blavozy 2025 | Bien-être Haute-Loire 43",
     description:
       "Salon de l'être à Blavozy (43, Haute-Loire) : bien-être, massage, réflexologie, énergétique, lithothérapie, coaching, conférences, ateliers, art divinatoire, tarot, astrologie, développement personnel, soins naturels, spiritualité, et plus. Proche Le Puy-en-Velay.",
-    url: "https://votre-domaine.fr",
+    url: "https://salon-etre.vercel.app",
     siteName: "Salon de l'être Blavozy",
     images: [
       {
